b56fe664053a7369d883968625285eaf591f6ac6
HGNB <672834580@qq.com>
2022-11-14 20:28:30 +0800

    4.2.1

--- src/views/monitor/refit/index.vue
+++ src/views/monitor/refit/index.vue
@@ -1,14 +1,43 @@
 <!-- 装备改型情况-->
 <template>
   <div>
-    <div
-      id="remodel_timeline"
-      style="cursor: pointer; width: 100%; height: 300px"
-    ></div>
-    <div
-      id="compared_stacked"
-      style="cursor: pointer; width: 100%; height: 300px"
-    ></div>
+    <el-card style="width: 95%; margin-left: 30px; margin-top: 10px">
+      <div style="width: 100%; background: #d2e9ff; border-radius: 10px">
+        <p
+          style="
+            font-family: Arial;
+            font-size: 16px;
+            font-weight: 600;
+            display: inline-block;
+            margin-left: 20px;
+          "
+        >
+          改型时间线
+        </p>
+      </div>
+      <div
+        id="remodel_timeline"
+        style="cursor: pointer; width: 100%; height: 300px"
+      ></div>
+
+      <div style="width: 100%; background: #d2e9ff; border-radius: 10px">
+        <p
+          style="
+            font-family: Arial;
+            font-size: 16px;
+            font-weight: 600;
+            display: inline-block;
+            margin-left: 20px;
+          "
+        >
+          对比堆叠图
+        </p>
+      </div>
+      <div
+        id="compared_stacked"
+        style="cursor: pointer; width: 100%; height: 300px"
+      ></div>
+    </el-card>
   </div>
 </template>
 
@@ -56,6 +85,7 @@ export default {
       const m = (dt.getMonth() + 1 + "").padStart(2, "0")
       return `${y}.${m}`
     },
+
     //获取数据 渲染 改型时间线折线图
     getData2() {
       listDataAll().then((response) => {
@@ -142,10 +172,10 @@ export default {
     set_Option_remodel_timeline() {
       // 改型时间线折线图
       let option = {
-        tooltip: {
-          show: true,
-          trigger: "axis",
-        },
+        // tooltip: {
+        //   show: true,
+        //   trigger: "axis",
+        // },
         xAxis: {
           type: "category",
           boundaryGap: false,
@@ -220,91 +250,91 @@ export default {
       }
       console.log("plane_remodel_time:", this.plane_remodel_time)
       console.log("max:", max)
-      let se = {
-        name: "",
-        type: "bar",
-        stack: "total",
-        label: {
-          show: true,
-        },
-        emphasis: {
-          focus: "series",
-        },
-        data: [],
-      }
-      for (var i = 0; i < this.plane_remodel_time.length; i++) {
-        se.data.push(1)
-      }
+
       for (var x = 0; x <= max; x++) {
+        let se = {
+          name: "第" + (x + 1) + "时间段",
+          type: "bar",
+          stack: "total",
+          label: {
+            show: true,
+          },
+
+          emphasis: {
+            focus: "series",
+          },
+          data: [],
+        }
+        for (var i = 0; i < this.plane_remodel_time.length; i++) {
+          se.data.push(0)
+        }
         this.se_stacked.push(se)
       }
 
       console.log("se_stacked", this.se_stacked)
-      //this.se_stacked[3].data[0]++
 
+      let aaa = []
+      aaa = JSON.parse(JSON.stringify(this.se_stacked))
       //循环所有质量问题表
-      // for (var j = 0; j < this.totalDataList1.length; j++) {
-      //   //循环飞机改型时间表
-      //   for (var y = 0; y < this.plane_remodel_time.length; y++) {
-      //     //找到对应机型
-      //     if (
-      //       this.plane_remodel_time[y].planeType ===
-      //       this.totalDataList1[j].planeType
-      //     ) {
-      //       var key = -1
-      //       //循环对应机型的改型时间数组
-      //       for (
-      //         var z = 0;
-      //         z < this.plane_remodel_time[y].remodelDate.length;
-      //         z++
-      //       ) {
-      //         //找到对应的时间间隔 (比他稍大一个)
-      //         if (
-      //           this.dateFormat(this.totalDataList1[j].devHappenTime) <
-      //           this.plane_remodel_time[y].remodelDate[z]
-      //         ) {
-      //           key = z
-      //           break
-      //         }
-      //       }
-      //       if (key === -1) {
-      //         key = this.plane_remodel_time[y].remodelDate.length
-      //       }
-      //       this.se_stacked[key].data[y]++
-      //     }
-      //   }
-      // }
-
-      console.log("se_stacked", this.se_stacked[0].data[0])
-      //this.se_stacked[0].data[0]++
-      let aaa=[];
-      /* for(let i=0;i<this.se_stacked.length;i++){
-          aaa.push(this.se_stacked[i]);
-      } */
-
-      aaa.push(JSON.parse(JSON.stringify(this.se_stacked)))
-      //aaa[0].data[0]++;
-      console.log(aaa);
-      console.log("se_stacked2222", this.se_stacked)
+      for (var j = 0; j < this.totalDataList1.length; j++) {
+        //循环飞机改型时间表
+        for (var y = 0; y < this.plane_remodel_time.length; y++) {
+          //找到对应机型
+          if (
+            this.plane_remodel_time[y].planeType ===
+            this.totalDataList1[j].planeType
+          ) {
+            var key = -1
+            //循环对应机型的改型时间数组
+            for (
+              var z = 0;
+              z < this.plane_remodel_time[y].remodelDate.length;
+              z++
+            ) {
+              //找到对应的时间间隔 (比他稍大一个)
+              if (
+                this.dateFormat(this.totalDataList1[j].devHappenTime) <
+                this.plane_remodel_time[y].remodelDate[z]
+              ) {
+                key = z
+                break
+              }
+            }
+            if (key === -1) {
+              key = this.plane_remodel_time[y].remodelDate.length
+            }
+            aaa[key].data[y]++
+          }
+        }
+      }
+      console.log(aaa)
+      for (var q = 0; q < aaa.length; q++) {
+        for (var p = 0; p < aaa[q].data.length; p++) {
+          if (aaa[q].data[p] === 0) aaa[q].data[p] = ""
+        }
+      }
+      this.se_stacked = aaa
     },
     //初始化 对比堆叠图
     init_Chart_compared_stacked() {
-      console.log(this.se_stacked[3].data[0]);
-      
       let compared_stacked_chart = echarts.init(
         document.getElementById("compared_stacked")
       )
       compared_stacked_chart.resize() //自适应大小
-      compared_stacked_chart.setOption(this.set_Option_compared_stacked(),true)
+      compared_stacked_chart.setOption(this.set_Option_compared_stacked(), true)
     },
     //初始化 对比堆叠图
     set_Option_compared_stacked() {
       // 对比堆叠图
       let option = {
-        tooltip: {
-          show: true,
-          trigger: "axis",
-        },
+        // tooltip: {
+        //   show: true,
+        //   trigger: "axis",
+        //   axisPointer: {
+        //     type: "shadow",
+        //   },
+        // },
+        legend: {},
         xAxis: {
           type: "value",
         },
