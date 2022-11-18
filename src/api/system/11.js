d2a3f1dd331c0391af51059da3ef656daee3773a
liuzhiyunlzy/liuzhiyunlzy <2281548983@qq.com>
2022-11-18 20:58:33 +0800

    前端更新

--- src/api/system/11.js
+++ src/api/system/11.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 
-// 查询8:装备部署数据列表
+// 查询11:装备部署数据列表
 export function list11(query) {
   return request({
     url: '/system/11/list',
@@ -9,7 +9,7 @@ export function list11(query) {
   })
 }
 
-// 查询8:装备部署数据详细
+// 查询11:装备部署数据详细
 export function get11(id) {
   return request({
     url: '/system/11/' + id,
@@ -17,7 +17,7 @@ export function get11(id) {
   })
 }
 
-// 新增8:装备部署数据
+// 新增11:装备部署数据
 export function add11(data) {
   return request({
     url: '/system/11',
@@ -26,7 +26,7 @@ export function add11(data) {
   })
 }
 
-// 修改8:装备部署数据
+// 修改11:装备部署数据
 export function update11(data) {
   return request({
     url: '/system/11',
@@ -35,7 +35,7 @@ export function update11(data) {
   })
 }
 
-// 删除8:装备部署数据
+// 删除11:装备部署数据
 export function del11(id) {
   return request({
     url: '/system/11/' + id,
