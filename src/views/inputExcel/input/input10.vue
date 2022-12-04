c895c4f2b4bbf31d236287f061d4b7e98b520470
mcr7283 <772888003@qq.com>
2022-12-04 19:59:22 +0800

    lfb 添加详情修改按钮 修改自适应大小

--- src/views/inputExcel/input/input10.vue
+++ src/views/inputExcel/input/input10.vue
@@ -43,7 +43,7 @@
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="List" @selection-change="handleSelectionChange" :default-sort="defaultSort" @sort-change="handleSortChange">
+    <el-table v-loading="loading" :data="List" @selection-change="handleSelectionChange" :default-sort="defaultSort" @sort-change="handleSortChange" class="myTable">
       <el-table-column type="selection" width="55" align="center" />
       <el-table-column label="序号" align="center" prop="id" />
       <el-table-column label="产品名称" align="center" prop="partsName" :show-overflow-tooltip="true" sortable="custom" :sort-orders="['descending', 'ascending']"/>
